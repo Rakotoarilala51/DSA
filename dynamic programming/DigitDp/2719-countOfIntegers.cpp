f02cb6ec75fcf7d35121c66c31627c0ac8ab731c
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-29 10:36:35 +0300

    investigation digit dp

---
+++ dynamic programming/DigitDp/2719-countOfIntegers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        int count(string num1, string num2, int min_sum, int max_sum) {
+            
+        }
+};
--- dynamic programming/DigitDp/investigation.cpp
+++ dynamic programming/DigitDp/investigation.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-string s;
-int dp(int index, bool last, int sum){
-    if(index==s.size()){
-        return sum;
+string n;
+int a,b, k;
+int dp(int index, bool last, int sum_mod, int sum_dig_mod){
+    if(index==n.size()){
+        if(sum_mod==0 && sum_dig_mod==0){
+            return 1;
+        }
+        return 0;
     }
-    int till = last?s[index]-'0':9;
+    int till = last?n[index]-'0':9;
     int ans=0;
     for(int digit=0; digit<=till; digit++){
-        ans+=dp(index+1, last && digit==till, sum+digit);
+        ans+=dp(index+1, last && digit==till, ((sum_mod*10)+digit)%k, (sum_dig_mod+digit)%k);
     }
     return ans;
 }
-int f(int _n){
-    s=to_string(_n);
-    return dp(0,1,0);
+int solve(int _n){
+    n=to_string(_n);
+    return dp(0, 1,0,0);
 }
+
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    while(1){
-        int a,b;
-        cin>>a>>b;
-        if(a==-1 && b==-1) break;
-        cout<<f(b)-f(a-1)<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>k;
+        cout<<solve(b)-solve(a-1)<<endl;
     }
- 
+    
     return 0;
 }
