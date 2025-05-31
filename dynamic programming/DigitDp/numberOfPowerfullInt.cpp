d0ebb00588a0fe1f360fedb00318b8d149663a24
Rakotoarilala51 <maitreaina51@gmail.com>
2025-05-31 21:49:26 +0300

    nothing

--- dynamic programming/DigitDp/numberOfPowerfullInt.cpp
+++ dynamic programming/DigitDp/numberOfPowerfullInt.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
@@ -9,42 +7,33 @@ public:
     string s;
     int lim;
     int x;
-    int index;
-    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s)
-    {
+    int memo[150][2];
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s){
         this->s = s;
         this->lim = limit;
         return count(finish)-count(start-1);
     }
-    long long count(long long _n)
-    {
+    long long count(long long _n){
         this->n = to_string(_n);
         if(s.size()>n.size()) return 0;
-        this->x = n.size() - s.size();
-        
+        memset(memo, -1, sizeof(memo));
+        this->x = n.size() - s.size();       
         return dp("",0, 1);
     }
-    long long dp(string res, int index, bool last)
-    {
+    long long dp(string res, int index, bool last){
         if (index == n.size())
-        {
-            
+        {   
             //cout<<res<<endl;
             if (res.substr(x, n.size()) == s){
-                cout<<res<<endl;
+                //cout<<res<<endl;
                 return 1;
-            }
-               
-               
+            }              
             return 0;
         }
-        this->index=min(lim, n[index]-'0');
-        int till = last ? n[index]-'0':lim;
+        int till = last ? min(lim, n[index]-'0'):lim;
         int ans = 0;
-        for (int digit = 0; digit <= lim; digit++)
-        {
-            
-            ans += dp(res+to_string(digit), index + 1, last && till == lim);
+        for (int digit = 0; digit <= till; digit++){    
+            ans += dp(res+to_string(digit), index + 1, last && digit == till);
         }
         return ans;
     }
@@ -52,8 +41,8 @@ public:
 int main()
 {
     Solution solution;
-    string s = "124";
-    long long start = 1, finish = 6000;
-    int limit = 4;
+    string s = "10";
+    long long start =15, finish = 2500;
+    int limit = 6;
     cout << solution.numberOfPowerfulInt(start, finish, limit, s);
 }
